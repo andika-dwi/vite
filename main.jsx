94c806000afbdc32d8dac9341ce905ff5a6f219f
Andika Dwi Putra <andikadp20@gmail.com>
2025-06-08 16:58:42 +0700

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome CSS (terminal npm install @fortawesome/fontawesome-free)
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App 
+    
+    
+    
+    />
+  </StrictMode>,
+
+
+
+
+)
